61c0e7bc54dd106d503cee1de56209228d3356f1
Frantisek Havluj <moskyt@rozhled.cz>
2012-10-09 09:33:07 +0200

    better api for two_charts; added single chart

--- lib/guttapercha.rb
+++ lib/guttapercha.rb
@@ -1,13 +1,13 @@
 module LaTeX
-  
+
   def self.escape(s)
     return "" unless s
     s.gsub("_", "\\_").gsub("#", "\\#")
   end
-  
-  def self.two_charts(file1, file2, caption)
+
+  def self.two_charts(file1, file2, caption, options = {})
     %{
-       \\begin{figure}
+       \\begin{figure}#{options[:placement] ? "[#{options[:placement]}]" : ''}
           \\begin{minipage}[!t]{0.49\\textwidth}
             \\input{#{file1}}
           \\end{minipage}
@@ -18,5 +18,14 @@ module LaTeX
         \\end{figure}
     }
   end
-  
+
+  def self.chart(file1, caption, options = {})
+    %{
+       \\begin{figure}#{options[:placement] ? "[#{options[:placement]}]" : ''}
+          \\input{#{file1}}
+          \\caption{#{caption}}
+        \\end{figure}
+    }
+  end
+
 end
